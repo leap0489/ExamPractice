fce98b6374fb2e325f0133f1ee4387036c1fe512
leap0489 <eapx0489@mylaurier.ca>
2024-04-15 17:13:46 -0400

    Changes

--- wp-content/themes/gv/customElements.js
+++ wp-content/themes/gv/customElements.js
@@ -8,7 +8,7 @@ customElements.define("x-year", CopyrightYear);
 
 class TwoSidedMarket extends HTMLElement{
     connectedCallback(){
-        this.innerHTML = `<a href="page-1">Page 1</a>&nbsp;<a href="page-2">Page 2</a>`;
+        this.innerHTML = `<a href="page-1" class="button">Page 1</a>&nbsp;<a href="page-2">Page 2</a>`;
     }
 }
 
